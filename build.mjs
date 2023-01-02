b0f6b3d5a44200b1dd4d85b032703f416393ff36
Dylan Staley <88163+dstaley@users.noreply.github.com>
2023-01-01 18:31:42 -0800

    fix baseUrl for vercel previews

--- build.mjs
+++ build.mjs
@@ -1,3 +1,8 @@
 import build from "@dstaley/zola-wasm";
 
-await build();
+const baseUrl =
+  process.env.VERCEL_ENV === "production"
+    ? "https://dstaley.com"
+    : `https://${process.env.VERCEL_URL}`;
+
+await build(".", baseUrl);
